781cf4f5021269e1c691660cfc14152a13f27cff
Alexalom99 <alexalom1999@gmail.com>
2025-03-26 18:45:40 +0100

    ex2 done

--- ex2.js
+++ ex2.js
@@ -3,7 +3,7 @@ function boton1() {
 }
 
 function boton2() {
-  document.querySelector("#panel").style.backgroundColor = "rgb(255, 0, 0)";
+  document.querySelector("#panel").style.backgroundColor = "rgb(0, 0, 255)";
 }
 
 function asociamePorFavor() {
@@ -11,7 +11,10 @@ function asociamePorFavor() {
     "Gracias por asociar la función al botón 3!";
 }
 
+
 function calcularLetrasTitulo() {
   // 1. Accede a la propiedad textContent del nodo h1. Guarda esa información en una variable
+  let texth1 = document.querySelector("h1").textContent;
   // 2. Usa el método adecuado para obtener el número de carácteres del string en textContent. Pon ese valor en el textContent del #panel
+  document.querySelector("#panel").textContent = texth1.length;
 }
